7ffc74b32036fbcf19fe3799cee2f196619b4ea0
roymck <lilmayo495@gmail.com>
2020-02-19 15:04:46 -0800

    inputs

---
+++ labs/traingle.c
@@ -0,0 +1,10 @@
+//Matthew Garcia Lab 1: Triangle Input
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Intput 3 positive integers from the terminal to determine if they are valid sides of a triangle");
+
+	int side_1, side_2, side_3;
+}
