f64bffa6bad8c47d936e12680fa939d1805fff2b
Matthew <mgarcia495@yahoo.com>
2020-02-23 21:26:17 -0800

    finnished

--- labs/triangle.c
+++ labs/triangle.c
@@ -1,49 +1,69 @@
 //Matthew Garcia Lab 1: Triangle Input
 
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	printf("Intput 3 positive integers from the terminal to determine if they are valid sides of a triangle \n");
 
-	int side_1, side_2, side_3;
+	double side_1, side_2, side_3;
 
 	printf("\nEnter the first side \n");
-	scanf("%d", &side_1);
-
+	scanf("%lf", &side_1);
 
 	printf("\nEnter the second side \n");
-	scanf("%d", &side_2);
-
+	scanf("%lf", &side_2);
 
 	printf("\nEnter the third side \n");
-	scanf("%d", &side_3);
+	scanf("%lf", &side_3);
+
+	double sum = (side_1 + side_2 + side_3) / 2;
+	double area = (sum * (sum-side_1) * (sum - side_2) * (sum - side_3) );
 
 	if(side_1 == side_2 && side_1 == side_3)
 	{
-		printf("\nEqualatoral\n");
-
-		printf("\nFirst side %d \n", side_1);
-		printf("\nSecond side %d \n", side_2);
-		printf("\nThird side %d \n", side_3);
+		printf("\nEqualatoral");		
+		printf("\nFirst side: %f", side_1);
+		printf("\nSecond side: %f", side_2);		
+		printf("\nThird side: %f\n", side_3);
+		printf("Area: %f\n", sqrt(area) );
 	}
 
 	else if(side_1 == side_2 || side_1 == side_3 || side_2 == side_3)
 	{
-		printf("\nIsoceles\n");
-
-		printf("\nFirst side %d \n", side_1);
-		printf("\nSecond side %d \n", side_2);
-		printf("\nThird side %d \n", side_3);
+		printf("\nIsoceles");
+		printf("\nFirst side: %f", side_1);
+		printf("\nSecond side: %f", side_2);
+		printf("\nThird side: %f\n", side_3);
+		printf("Area: %f\n", sqrt(area) );
 	}
 
 	else
 	{
-		printf("\nScalene\n");
-
-		printf("\nFirst side %d \n", side_1);
-		printf("\nSecond side %d \n", side_2);
-		printf("\nThird side %d \n", side_3);
-
+		printf("\nScalene");
+		printf("\nFirst side: %f", side_1);
+		printf("\nSecond side: %f", side_2);
+		printf("\nThird side: %f\n", side_3);
+		printf("Area: %f\n", sqrt(area) );
 	}
 }
+
+/*
+Intput 3 positive integers from the terminal to determine if they are valid sides of a triangle 
+
+Enter the first side 
+2
+
+Enter the second side 
+2
+
+Enter the third side 
+2
+
+Equalatoral
+First side: 2
+Second side: 2
+Third side: 2
+Area: 1.732051 
+ */
