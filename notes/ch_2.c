90ec9d46ac77c1abfcb47bee39e8acfbcec7132a
roymck <lilmayo495@gmail.com>
2020-02-25 17:33:06 -0800

    end of lecture notes

--- notes/ch_2.c
+++ notes/ch_2.c
@@ -1,3 +1,4 @@
+// loops
 #include <stdio.h>
 
 int main()
@@ -9,116 +10,137 @@ int main()
 	a % b >= 1 || c > 5 && !a
 
 		5 % s >= 1 || -3 > 5 && ! 5
-}
-
-//if statement
-if(<expression>)
-{
-	<statement>;
-}
 
 
-
-// if_else statement
-if(<expression>)
-{
-	<statement>
-}
-
-else
-{
-	<statement>
-}
+		//if statement
+		if(<expression>)
+		{
+			<statement>;
+		}
 
 
 
-// nested if_else if statment:
-if(<expression>)
-{
-	<statement>;
-}
+	// if_else statement
+	if(<expression>)
+	{
+		<statement>
+	}
 
-else if(expression>)
-{
-	<statement>;
-}
-
-else
-{
-	<statment>;
-	// if everthing fails, the else will be it's default
-}
+	else
+	{
+		<statement>
+	}
 
 
 
-// switch statment:
-switch (<expression>)
-{
-	// must be ints or chars
-
-	case <c1>:
+	// nested if_else if statment:
+	if(<expression>)
+	{
 		<statement>;
-		break;
+	}
 
-	case <c2>:
+	else if(expression>)
+	{
 		<statement>;
-		break;
+	}
 
-	case <c3>:
+	else
+	{
+		<statment>;
+		// if everthing fails, the else will be it's default
+	}
+
+
+
+	// switch statment:
+	switch (<expression>)
+	{
+		// must be ints or chars
+
+		case <c1>:
+			<statement>;
+			break;
+
+		case <c2>:
+			<statement>;
+			break;
+
+		case <c3>:
+			<statement>;
+			break;
+
+		default:
+			<statement>;
+			break;
+	}
+
+
+	//while loop  pre-test loop
+	while (<expression)
+	{
 		<statement>;
-		break;
+	}
+	n = 2;
+	while(n <= 10)
+	{
+		printf("%d", n);
+	}
 
-	default:
+
+	//for loop pre-test loop
+	for(initialization; <expression>; <update)
+	{
 		<statement>;
-		break;
+	}
+
+
+	for(int i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+
+
+	//do_while loop: post-test loop
+	do
+	{
+		<statement>;
+	}
+	while(<expression);
+	/*
+	   continue statement:
+	   - causes a loop to stop its current iteration and being the next one
+
+	   break statement:
+	   - causes a loop to terminate and the program jumps to the state,ent following the loop.
+
+	   return statement:
+	   - terminates execution of a function
+
+	   exit(n) function
+	   - causes a program to terminate 
+	   exit(0) means normal termination
+	   */
+
+
+	/* getchar() function:
+	   - reads a character from keyboard
+	   - refers the corresponding integer value as type int
+
+	   int iochar;
+	   iochar = getchar();
+	   ----------------------------- 
+
+	   putchar(n) function:
+	   - takes an argument of type int
+	   - outputs the corresponding ACII Character
+
+	   int iochar;
+
+	   iochar = getchar();
+
+	   putchar(iochar);
+
+
+EOF : signifies end of input
+*/
 }
-
-
-//while loop
-while (<expression)
-{
-	<statement>;
-}
-n = 2;
-while(n <= 10)
-{
-	printf("%d", n);
-}
-
-
-//for loop
-for(initialization; <expression>; <update)
-{
-	<statement>;
-}
-
-
-for(int i = 0; i <= 10; i++)
-{
-	printf("%d", i);
-}
-
-
-//do_while loop:
-
-do
-{
-	<statement>;
-}
-while(<expression);
-}
-
-/*
-   continue statement:
-   - causes a loop to stop its current iteration and being the next one
-
-   break statement:
-   - causes a loop to terminate and the program jumps to the state,ent following the loop.
-
-   return statement:
-   - terminates execution of a function
-
-   exit(n) function
-   - causes a program to terminate 
-   exit(0) means normal termination
-   */
