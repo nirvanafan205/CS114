3241c6bf6d51bb79dc31cb542580b628eb9e1007
roymck <lilmayo495@gmail.com>
2020-03-03 14:44:22 -0800

    3/2 notes

--- notes/ch_3.c
+++ notes/ch_3.c
@@ -60,4 +60,113 @@ Formated output|
 	int x;
 	(float) x;
  
+ ----------------------------------------------------------------------------------------------------
+
+ Automatic type conversion:|
+ ---------------------------
+
+ 1) Result of relational and logical operations are int (0 or 1)
+ 2) char and short are "promoted" to int.
+ 3) formixed types convert values to a "domainating type"
+
+int i;
+short s;
+long l;
+float f;
+
+f = s * l + i;
+
+
+	long double		most dominant
+
+	double
+
+	float
+
+	unsigned long
+
+	long
+
+	unsigned int
+
+	int			least dominant
+----------------------------------------------------------------------------------------------------
+Functions|
+--------------------------------------------------------------------------------------------------------------
+
+		- A modual program is composed of many functions
+
+	#inlcude <stdio.h>
+
+	float calcAverage(float num1, float num2, float num3);
+
+	int main(void)
+{
+
+	float num1, num2, num3, average;
+
+	printf("Enter 3 numbers:");
+
+	scanf("%f %f %f", &num1, &num2, &num3);
+
+	average = calcAverage(num1,num2,num3);
+
+	printf("Average = %6.2f\n", average);
+
+	return 0;
+}
+
+	float calcAverage(float num1, float num2, float num3)
+{
+	return (num1 + num2 + num3) / 3;
+}
+
+- scope of a variable is the part of the program
+in which the variable can be referenced
+
+	Local Variable:
+	- is a variable declared inside of ablock
+	- can only be referenced inside of its block
+
+	Global Variable:
+	-is a variable declared outtside all functions
+	- can be referenced from its point of declaration to the end of the program
+	-By default its initialized to zero
+
+	* Try to avoid using global variables in programs because of "side effect" problems
+	
+---------------------------------------------------------------------------------------------------------------
+	
+	
+mathematical functions
+
+	#include <math.h>
+
+floor(1.8) -> 1.0	exp(x)		pow(x,y)	x ^y
+
+floor(4.1) -> 4.0	log(x)		sqrt(x)
+			log10(x)
+					ceil(3.8) -> 4.0
+					ceil(8.1) -> 9.0
+tabs(-4.1) -> 4.1	
+
+
+character manipulation functions/ macros
+
+		#include <ctype.h>
+		
+		islower(ch)		isupper(ch)		isspace(ch)
+		
+		islower('a') -> 1	isupper('T') -> 1	isspace('\t') -> 1
+
+		islower('5') -> 0	isupper('$') -> 0	isspace('\n') -> 1
+
+		isalpha(ch)		isdigit(ch)
+
+		isalpha('b') -> 1	isdigit('6') -> 1	toupper(ch)
+
+		isalpha('S') -> 1	isdigit('=') -> 0	toupper('d') -> 'D'	toupper('$') -> '$'
+
+		isalpha(' ') -> 0				toupper('U') -> 'U'
+
  */
