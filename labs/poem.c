4108bb3a85f70d15a746fcf27396d6728e820e7d
Matthew <mgarcia495@yahoo.com>
2020-03-08 23:25:08 -0700

    first commit

--- labs/poem.c
+++ labs/poem.c
@@ -1,10 +1,33 @@
 /* Garcia, Matthew 
- Lab 2 poem
- 
- */
+   Lab 2 poem
+
+*/
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello World\n");
+	char input;
+	while( input = getchar() ) 
+	{
+		if(input == EOF)
+		{
+			break;
+		}
+		else if(input == 32)
+		{
+			printf("\n");
+		}
+		else if(input >=33 && input <= 64)
+		{
+			printf("");
+		}
+		else if(input >= 9 && input <= 11)
+		{
+			printf("");
+		}
+		else
+		{
+		putchar(input);	
+		}
+	}
 }
