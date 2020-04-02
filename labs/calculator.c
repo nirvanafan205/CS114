3f6b937e1369b36911d6b62c8143e5d56bf474eb
Matthew <mgarcia495@yahoo.com>
2020-04-02 09:48:10 -0700

    first push

---
+++ labs/calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/*
+   void addition();	
+
+   void subraction();
+
+   void multiple();
+
+   void division();
+
+   void modulo();
+   */
+int main()
+{
+	printf("Enter 2 numbers and an operator for basic calculator computation \n");
+
+	char input;
+
+	while(input = getchar() )
+	{
+
+		if(input == EOF)
+		{
+			break;
+		}
+	}
+}
+
+/*
+   void addition()
+   {
+
+   }
+
+   void subraction()
+   {
+
+   }
+
+   void multiple()	
+   {
+
+   }
+
+   void division()
+   {
+
+   }
+
+   void modulo()
+   {
+
+   }
+
+*/
