9643ae28c232b8504ac75923f99071acc1c27362
Matthew <mgarcia495@yahoo.com>
2020-04-02 15:09:45 -0700

    check

--- labs/test.c
+++ labs/test.c
@@ -1,60 +1,26 @@
 #include <stdio.h>
 
-
 int main()
 {
-<<<<<<< HEAD
 	char input;
+	int num;
 
 	while(input = getchar() )
 	{
 		int ascii = (int)input;
-
 		if(input == EOF)
 		{
 			break;
 		}
-		
+
 		else if(ascii >= 48 && ascii <= 57)
 		{
-			int num = ascii;
-
 			num = (2 * ascii) - num;
 
-
 			putchar(num);
-
 		}
 
-		else
-		{
-			printf("\n");
-		}
 	}
-=======
-        char input;
 
-        while(input = getchar() )
-        {
-                int ascii = (int)input;
-
-                if(input == EOF)
-                {
-                        break;
-                }
-
-                else if(ascii >= 48 && ascii <= 57)
-                {
-                        int num = ascii - 48;
-
-                        putchar(num);
-                }
-
-                else
-                {
-                        printf("\n");
-                }
-        }
->>>>>>> dcc65d655ffeb4b658a637df8f1910134023ddb4
+	printf("%d", num);
 }
-
