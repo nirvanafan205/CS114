e24d1151698acc8f5aafe6fad6426cf3f693b417
Matthew <mgarcia495@yahoo.com>
2020-04-05 16:01:18 -0700

    two numbers

--- labs/test.c
+++ labs/test.c
@@ -3,29 +3,26 @@
 int main()
 {
 	char input;
-	int num;
-	int num_2;
+	int num,num_2;
+	char op;
+
 	while(input = getchar())
 	{
-		int ascii = (int)input;
-
-		if(input == EOF || input == 32) /* 32 = space, it breaks from the loop once ascii is spacebar */
+		if(input == EOF || input == 32) 
 		{
 			break;
 		}
 
-		else if(ascii >= 48 && ascii <= 57)
+		else if(input >= 48 && input <= 57)
 		{
-			num = (ascii - 48) + num * 10;
+			num = (input - 48) + num * 10;
 		}
 
 	}
-	printf("%d ", num); /* space added for next number */
+	printf("%d ", num); 
 
 	while(input = getchar() )
 	{
-		/* int ascii_ = (int)input; ---> adding this would break the code, adding millions to num and num_2 */
-
 		if(input == EOF || input == 32)
 		{
 			break;
@@ -33,9 +30,9 @@ int main()
 
 		else if(input >= 48 && input <= 57)
 		{
-			printf("why does this work without adding millions to num");
-
-			/* num_2 = (ascii - 48) + num_2 * 10;  would also add millions to num and num_2 */ 
+			num_2 = (input - 48) + num_2 * 10;
 		}
 	}
+
+	printf("%d", num_2);
 }
