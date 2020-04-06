bfb7066d17b9f458d2ee7cd78dcf997404d2d1bf
Matthew <mgarcia495@yahoo.com>
2020-04-05 20:41:42 -0700

    finished

--- labs/calculator.c
+++ labs/calculator.c
@@ -1,56 +1,166 @@
+/* Garcia, Matthew
+ * Lab 3: Calculator
+ * March 30
+ */
 #include <stdio.h>
 
-/*
-   void addition();	
+addition(int num,char op, int num_2);
 
-   void subraction();
+modulo(int num,char op, int num_2); /* functions to do arithmitic */
 
-   void multiple();
+subtraction(int num,char op, int num_2);
 
-   void division();
+division(int num,char op, int num_2);
 
-   void modulo();
-   */
+multiplication(int num,char op, int num_2);
+
 int main()
 {
-	printf("Enter 2 numbers and an operator for basic calculator computation \n");
+	char input, op;
+	int num,num_2;
 
-	char input;
-
-	while(input = getchar() )
+	while(input = getchar())
 	{
-
-		if(input == EOF)
+		if(input == EOF || input == 32) 
 		{
 			break;
 		}
+
+		else if(input == 45)
+		{
+			printf("no negatives"); /*checking for negatives */
+
+			exit(0);
+		}
+
+		else if(input >= 48 && input <= 57)
+		{
+			num = (input - 48) + num * 10;
+		}
+	}
+
+	while(input = getchar() )
+	{
+		if(input == EOF || input == 32)
+		{
+			break;
+		}
+
+		switch(input)		/* switch statement to determine operator sign */
+		{
+			case 37:
+				op = 37;
+				break;
+
+			case 42:
+				op = 42;
+				break;
+
+			case 43:
+				op = 43;
+				break;
+
+			case 45:
+				op = 45;
+				break;
+
+			case 47:
+				op = 47;
+				break;
+
+			default:
+				printf("input error");
+
+				exit(0);
+		}
+	}
+
+	while(input = getchar() )
+	{
+		if(input == EOF || input == 32)
+		{
+			break;
+		}
+
+		else if(input == 45)
+		{
+			printf("no negatives");
+			exit(0);
+			break;
+		}
+
+		else if(op == 47 && input == 48) /* checks to not divide by zero */
+		{
+			printf("no dividing by 0");
+			exit(0);
+		}
+
+		else if(input >= 48 && input <= 57)
+		{
+			num_2 = (input - 48) + num_2 * 10;
+		}
+	}
+
+	printf("%d %c %d", num, op, num_2);
+	printf(" = ");
+
+	if(op == 37)
+	{
+		num = modulo(num,op,num_2);
+
+		printf("%d ", num);
+	}
+
+	else if(op == 42)
+	{
+		num = multiplication(num,op,num_2);
+
+		printf("%d ",num);
+	}
+
+	else if(op == 43)
+	{
+		num = addition(num, op, num_2);
+
+		printf("%d ", num);
+	}
+
+	else if(op == 45)
+	{
+		num = subtraction(num, op, num_2);
+
+		printf("%d ", num);
+	}
+
+	else if(op == 47)
+	{
+		num = division(num,op,num_2);
+
+		printf("%d ", num);
 	}
 }
 
-/*
-   void addition()
-   {
+addition(int num,char op, int num_2)
+{
+	return num + num_2;
+}
 
-   }
+modulo(int num,char op, int num_2)
+{
+	return num % num_2;
+}
 
-   void subraction()
-   {
+subtraction(int num,char op, int num_2)
+{
+	return num - num_2;
+}
 
-   }
+division(int num,char op, int num_2)
+{
+	return num / num_2;
+}
 
-   void multiple()	
-   {
-
-   }
-
-   void division()
-   {
-
-   }
-
-   void modulo()
-   {
-
-   }
-
-*/
+multiplication(int num,char op, int num_2) /* declaration functions for arithmetic */
+{
+	return num * num_2;
+}
