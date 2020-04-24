7b36213f6fdbe739c5b75885db6dbaf2fc3d7076
pieface495 <nirvanafan205@gmail.com>
2020-04-23 23:42:42 -0700

    progress

--- labs/array_notation.c
+++ labs/array_notation.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *s1(char random[]);
+char s1(char random[] );
 
 int main()
 {
+    char random_string[40];
 
-	char random[40];
-
-
-
+    char helper[40] = s1(random_string[40]);
 }
 
-char *s1(char random[])
+char s1(char random[])
 {
-	int index = 0;
+    int index = 0;
 
-	char letter_[40];
+    char letter_[40];
 
-	char characters;
+    char characters;
 
 
-	while(characters = 'A' + (rand() % 26))
-	{
-		if(index == 40)
-		{
-			break;
-		}
+    while(characters = 'A' + (rand() % 26))
+    {
+        if(index == 40)
+        {
+            break;
+        }
 
-		letter_[index] = characters;
+        letter_[index] = characters;
 
-		index++;
-	}
+        index++;
+    }
+
+    for(int i = 0; i <= 40; i++)
+    {
+        putchar(letter_[i]);
+    }
 }
